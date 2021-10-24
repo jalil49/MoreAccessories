7eb6e5b6f14e4498e53a072d5e0fbbb2264c7996
jalil <63772680+jalil49@users.noreply.github.com>
2021-10-24 07:16:49 -0500

    fix reference

--- src/Core.MoreAccessories/Patches/Patches_Common.cs
+++ src/Core.MoreAccessories/Patches/Patches_Common.cs
@@ -24,63 +24,49 @@ namespace MoreAccessoriesKOI.Patches
             {
                 if (__instance.showAccessory != null && seal && value.Length != __instance.showAccessory.Length)
                 {
-                    MoreAccessories.Print("Please do not try to change showAccessory array size outside of MoreAccessories.ArraySync", BepInEx.Logging.LogLevel.Warning);
+                    MoreAccessories.Print($"Please do not try to change showAccessory array size outside of MoreAccessories.ArraySync ", BepInEx.Logging.LogLevel.Warning);
                     return false;
                 }
                 return true;
             }
         }
-        [HarmonyPatch(typeof(ChaControl), nameof(ChaControl.cusAcsCmp), MethodType.Setter)]
+        [HarmonyPatch(typeof(ChaInfo), nameof(ChaInfo.cusAcsCmp), MethodType.Setter)]
         internal class CusAcsCmpSetterPatch
         {
             internal static bool seal = true;
-            internal static bool Prefix(ChaControl __instance, ChaAccessoryComponent[] value)
+            internal static bool Prefix(ChaInfo __instance, ChaAccessoryComponent[] value)
             {
                 if (__instance.cusAcsCmp != null && seal && value.Length != __instance.cusAcsCmp.Length)
                 {
-                    MoreAccessories.Print("Please do not try to change cusAcsCmp array size outside of MoreAccessories.ArraySync", BepInEx.Logging.LogLevel.Warning);
+                    MoreAccessories.Print($"Please do not try to change cusAcsCmp array size outside of MoreAccessories.ArraySync ", BepInEx.Logging.LogLevel.Warning);
                     return false;
                 }
                 return true;
             }
         }
-        [HarmonyPatch(typeof(ChaControl), nameof(ChaControl.objAccessory), MethodType.Setter)]
+        [HarmonyPatch(typeof(ChaInfo), nameof(ChaInfo.objAccessory), MethodType.Setter)]
         internal class ObjAccessorySetterPatch
         {
             internal static bool seal = true;
-            internal static bool Prefix(ChaControl __instance, ChaAccessoryComponent[] value)
+            internal static bool Prefix(ChaInfo __instance, ChaAccessoryComponent[] value)
             {
                 if (__instance.objAccessory != null && seal && value.Length != __instance.objAccessory.Length)
                 {
-                    MoreAccessories.Print("Please do not try to change objAccessory array size outside of MoreAccessories.ArraySync", BepInEx.Logging.LogLevel.Warning);
+                    MoreAccessories.Print($"Please do not try to change objAccessory array size outside of MoreAccessories.ArraySync ", BepInEx.Logging.LogLevel.Warning);
                     return false;
                 }
                 return true;
             }
         }
-        [HarmonyPatch(typeof(ChaControl), nameof(ChaControl.objAcsMove), MethodType.Setter)]
+        [HarmonyPatch(typeof(ChaInfo), nameof(ChaInfo.objAcsMove), MethodType.Setter)]
         internal class ObjAcsMoveSetterPatch
         {
             internal static bool seal = true;
-            internal static bool Prefix(ChaControl __instance, GameObject[,] value)
+            internal static bool Prefix(ChaInfo __instance, GameObject[,] value)
             {
                 if (__instance.objAcsMove != null && seal && value.Length != __instance.objAcsMove.Length)
                 {
-                    MoreAccessories.Print("Please do not try to change objAcsMove array size outside of MoreAccessories.ArraySync", BepInEx.Logging.LogLevel.Warning);
-                    return false;
-                }
-                return true;
-            }
-        }
-        [HarmonyPatch(typeof(ChaControl), nameof(ChaControl.hideHairAcs), MethodType.Setter)]
-        internal class HideHairAcsSetterPatch
-        {
-            internal static bool seal = true;
-            internal static bool Prefix(ChaControl __instance, bool[] value)
-            {
-                if (__instance.hideHairAcs != null && seal && value.Length != __instance.hideHairAcs.Length)
-                {
-                    MoreAccessories.Print("Please do not try to change hideHairAcs array size outside of MoreAccessories.ArraySync", BepInEx.Logging.LogLevel.Warning);
+                    MoreAccessories.Print($"Please do not try to change objAcsMove array size outside of MoreAccessories.ArraySync ", BepInEx.Logging.LogLevel.Warning);
                     return false;
                 }
                 return true;
@@ -94,7 +80,22 @@ namespace MoreAccessoriesKOI.Patches
             {
                 if (__instance.infoAccessory != null && seal && value.Length != __instance.infoAccessory.Length)
                 {
-                    MoreAccessories.Print("Please do not try to change infoAccessory array size outside of MoreAccessories.ArraySync", BepInEx.Logging.LogLevel.Warning);
+                    MoreAccessories.Print($"Please do not try to change infoAccessory array size outside of MoreAccessories.ArraySync ", BepInEx.Logging.LogLevel.Warning);
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(ChaControl), nameof(ChaControl.hideHairAcs), MethodType.Setter)]
+        internal class HideHairAcsSetterPatch
+        {
+            internal static bool seal = true;
+            internal static bool Prefix(ChaControl __instance, bool[] value)
+            {
+                if (__instance.hideHairAcs != null && seal && value.Length != __instance.hideHairAcs.Length)
+                {
+                    MoreAccessories.Print($"Please do not try to change hideHairAcs array size outside of MoreAccessories.ArraySync ", BepInEx.Logging.LogLevel.Warning);
                     return false;
                 }
                 return true;
